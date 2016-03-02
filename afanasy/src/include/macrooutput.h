e941e12c082823578fcece65eae0c4252b455d95
Elie Michel <elie@risefx.com>
2016-03-02 18:30:28 +0100

    Add timing to AFERRORs

--- afanasy/src/include/macrooutput.h
+++ afanasy/src/include/macrooutput.h
@@ -2,13 +2,14 @@
 #include <errno.h>
 #include <iostream>
 #include <stdio.h>
+#include "../libafanasy/af.h"
 //#################### OUPUT ERRORS FORMATING ######################################
 
 //#define AFERROR( msg) {fprintf(stderr,"AFERROR: %s",msg);fprintf(stderr,"\n");fflush( stderr);}
-#define AFERROR( msg) { std::cerr << "AFERROR: " << msg << std::endl; std::cerr.flush();}
-#define AFERRAR( msg, ...) {fprintf(stderr,"AFERROR: ");fprintf( stderr, msg, __VA_ARGS__);fprintf(stderr,"\n");fflush( stderr);}
-#define AFERRPE( msg) {fprintf(stderr,"AFERROR: ");fflush( stderr);perror( msg );fprintf(stderr,"\n");fflush( stderr);}
-#define AFERRPA( msg, ...) {fprintf(stderr,"AFERROR: ");fprintf( stderr, msg, __VA_ARGS__);fflush( stderr);perror(0);fprintf(stderr,"\n");fflush( stderr);}
+#define AFERROR( msg) { std::cerr << af::time2str()<< ": AFERROR: " << msg << std::endl; std::cerr.flush();}
+#define AFERRAR( msg, ...) {fprintf(stderr,"%s: AFERROR: ",af::time2str().c_str());fprintf( stderr, msg, __VA_ARGS__);fprintf(stderr,"\n");fflush( stderr);}
+#define AFERRPE( msg) {fprintf(stderr,"%s: AFERROR: ",af::time2str().c_str());fflush( stderr);perror( msg );fprintf(stderr,"\n");fflush( stderr);}
+#define AFERRPA( msg, ...) {fprintf(stderr,"%s: AFERROR: ",af::time2str().c_str());fprintf( stderr, msg, __VA_ARGS__);fflush( stderr);perror(0);fprintf(stderr,"\n");fflush( stderr);}
 //
 //############## OUPUT INFO FORMATING, if AFOUTPUT only ##############################
 #ifdef AFOUTPUT
